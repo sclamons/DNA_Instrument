c19b99cc057616ae26cd0a51f198f7bf0b930975
mcscope <mcscope@gmail.com>
2015-07-27 23:39:03 -0700

    Replace random generation with calling a function for every note.  Could easily feed dna in now

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import time
 from pyo import *
 
-scale = midiToHz([60,62,64,65,67,69,71,72])
+# Useful pyo example lines
+
+# Randomly choose from a list.
+# rnd = Choice(choice=freqs, freq=[3,4])
+# Call after a time
+# a = CallAfter(callback, 2, [300,301])
+# Call repeatedly after intervals
+# p = Pattern(callback, .125)
+
 
 def play_music(server):
     freqs = midiToHz([60,62,64,65,67,69,71,72])
-    rnd = Choice(choice=freqs, freq=[3,4])
-    a = SineLoop(rnd, feedback=0.05, mul=.2).out()
+
+    wave = SineLoop(100, feedback=0.05, mul=.2).out()
+
+    def each_note():
+        wave.freq = freqs.pop(0)
+        freqs.append(wave.freq)
+
+    p = Pattern(each_note, .125)
+    p.play()
 
     time.sleep(100.000000)
 
