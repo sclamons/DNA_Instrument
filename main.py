6a9e476dcecf4a1b865d3f68d0a8b5162ab4c67b
mcscope <mcscope@gmail.com>
2015-07-27 23:28:50 -0700

    replace with a little more structure and a random number generator midi thing

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-from pyo import *
 import time
-s = Server().boot()
-f = Adsr(attack=.01, decay=.2, sustain=.5, release=.1, dur=5, mul=.5)
-a = Sine(440, 0, mul=1).out()
-s.start()
-f.play()
-time.sleep(4)
+from pyo import *
 
-s.stop()
+scale = midiToHz([60,62,64,65,67,69,71,72])
+
+def play_music(server):
+    freqs = midiToHz([60,62,64,65,67,69,71,72])
+    rnd = Choice(choice=freqs, freq=[3,4])
+    a = SineLoop(rnd, feedback=0.05, mul=.2).out()
+
+    time.sleep(100.000000)
+
+
+def main():
+    s = Server().boot()
+    s.start()
+
+    play_music(s)
+
+    s.stop()
+
+
+if __name__ == "__main__":
+    main()
