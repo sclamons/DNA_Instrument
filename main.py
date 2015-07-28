e748591920939638efce815ba08c0b2f5f4857f6
mcscope <mcscope@gmail.com>
2015-07-27 22:33:40 -0700

    switched to different example

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from pyo import *
+import time
 s = Server().boot()
+f = Adsr(attack=.01, decay=.2, sustain=.5, release=.1, dur=5, mul=.5)
+a = Sine(440, 0, mul=1).out()
 s.start()
-a = Sine(mul=0.01).out()
-s.gui(locals())
+f.play()
+time.sleep(4)
+
+s.stop()
